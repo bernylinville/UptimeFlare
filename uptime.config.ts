b375080dba1f9b1a64cdbafaffa124ac18a82685
Berny Linville <bernylinville@devopsthink.org>
2024-08-22 14:08:42 +0800

    update

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,11 +1,11 @@
 const pageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "bernylinville's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/bernylinville', label: 'GitHub' },
+    { link: 'https://blog.bernylinville.net/', label: 'Blog' },
+    { link: 'mailto:me@bernylinville.net', label: 'Email Me', highlight: true },
   ],
 }
 
